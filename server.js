1beb575def3b794ac10fecb986f2b44dbe29d32d
Amirhosein Mohammadisabet <amirhoseinmohammadisabet@gmail.com>
2022-10-15 23:42:46 +0330

    post method in express

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
 
-app.listen(8080,function(){
+app.listen(8080, function () {
 	console.log('listening on 8080')
 })
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 	//res.send("hello world")
 	res.sendfile(__dirname + '/index.html')
+})
+
+app.post("/quotes", (req, res) => {
+	console.log("Helooooooo!");
 })
